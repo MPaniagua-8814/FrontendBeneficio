ff5eab6f8b3206450d941f2cd67b2eb2d2643be0
MELANNIE JACQUELINE PANIAGUA LÓPEZ <106444648+MPaniagua-8814@users.noreply.github.com>
2023-05-25 14:22:14 -0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,7 +36,7 @@ const routes: Routes = [
 },
   {  ////nueva ruta para ver detalle
   //path: 'https://frontend-beneficio-fje3kl50a-mpaniagua-8814.vercel.app/bandeja-principal/detalle-pesaje/:idCuenta/:estadoActual/qr/:idParcialidad/info-qr/:idParcialidad', 
-  path: window.location.href+'bandeja-principal/detalle-pesaje/:idCuenta/:estadoActual/qr/:idParcialidad/info-qr/:idParcialidad', 
+  path: sessionStorage.getItem("ruta")+'bandeja-principal/detalle-pesaje/:idCuenta/:estadoActual/qr/:idParcialidad/info-qr/:idParcialidad', 
   component: InfoParcialidadQRComponent
 }
 ,
