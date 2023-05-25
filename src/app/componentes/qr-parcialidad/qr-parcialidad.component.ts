c7d04b754381e167ac274b68b2c06227bbfb609c
MELANNIE JACQUELINE PANIAGUA LÓPEZ <106444648+MPaniagua-8814@users.noreply.github.com>
2023-05-25 13:34:23 -0600

    Update qr-parcialidad.component.ts

--- src/app/componentes/qr-parcialidad/qr-parcialidad.component.ts
+++ src/app/componentes/qr-parcialidad/qr-parcialidad.component.ts
@@ -24,8 +24,8 @@ export class QrParcialidadComponent implements OnInit {
     //https://frontend-beneficio-fje3kl50a-mpaniagua-8814.vercel.app/bandeja-principal/detalle-pesaje/6/Cuenta Creada/qr/12/info-qr/12
 
     // url = '/bandeja-principal/detalle-pesaje/'+this.route.snapshot.paramMap.get("idCuenta")+'/'+this.route.snapshot.paramMap.get("estadoActual")+'/qr/'+this.route.snapshot.paramMap.get('idParcialidad')+'/info-qr/'+this.route.snapshot.paramMap.get('idParcialidad')
-    url = 'https://frontend-beneficio-fje3kl50a-mpaniagua-8814.vercel.app/bandeja-principal/detalle-pesaje/'+this.route.snapshot.paramMap.get("idCuenta")+'/'+this.route.snapshot.paramMap.get("estadoActual")+'/qr/'+this.route.snapshot.paramMap.get('idParcialidad')+'/info-qr/'+this.route.snapshot.paramMap.get('idParcialidad')
-    
+    //url = 'https://frontend-beneficio-fje3kl50a-mpaniagua-8814.vercel.app/bandeja-principal/detalle-pesaje/'+this.route.snapshot.paramMap.get("idCuenta")+'/'+this.route.snapshot.paramMap.get("estadoActual")+'/qr/'+this.route.snapshot.paramMap.get('idParcialidad')+'/info-qr/'+this.route.snapshot.paramMap.get('idParcialidad')
+    url = '/bandeja-principal/detalle-pesaje/'+this.route.snapshot.paramMap.get("idCuenta")+'/'+this.route.snapshot.paramMap.get("estadoActual")+'/qr/'+this.route.snapshot.paramMap.get('idParcialidad')+'/info-qr/'+this.route.snapshot.paramMap.get('idParcialidad')
    
     value=this.url+this.profile;
 
@@ -40,7 +40,7 @@ export class QrParcialidadComponent implements OnInit {
 
     // this.url = this.url + this.idParcialidad
     // this.url = 'adios'//this.url+this.idCuenta+'/'+this.estadoActual+'/info-qr/'+this.idParcialidad;  
-
+    console.log("ruta que tenemos = " , this.url);
     //para validar que el usuario esté autenticado
     if (!this.authService.isAuthenticated()) {
       this.router.navigate(['/login']);
